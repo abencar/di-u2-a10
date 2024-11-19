ec7d5c3b87f1413e27ca30c299372b61e65b2e10
Álvaro Benítez Carmona <76166064+abencar@users.noreply.github.com>
2024-11-19 13:09:41 +0100

    Hecho

--- src/app/Scoreboard.js
+++ src/app/Scoreboard.js
@@ -8,8 +8,12 @@ export default function Scoreboard() {
   });
 
   function handlePlusClick() {
-    player.score++;
+    setPlayer({
+      ...player,
+      score: player.score + 1,
+    });
   }
+
 
   function handleFirstNameChange(e) {
     setPlayer({
@@ -20,6 +24,7 @@ export default function Scoreboard() {
 
   function handleLastNameChange(e) {
     setPlayer({
+      ...player,
       lastName: e.target.value
     });
   }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Canvas from "./Canvas";
+import Scoreboard from "./Scoreboard.js";
+import Canvas2 from "./Canvas2.js";
 
 export default function Home() {
   return (
@@ -13,15 +16,14 @@ export default function Home() {
       <br />
       <br />
       <h1>Ejercicio 2</h1>
-      <Canvas />
+      <Canvas2 />
       <br />
       <br />
       <br />
       <br />
       <br />
       <h1>Ejercicio 3</h1>
-      
-
+      {/* <Canvas2 /> */}
     </div>
   );
 }
